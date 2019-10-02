244572c355bbdcceaeff031cf229b3ddde42da82
mfratassi <mfratassi@gmail.com>
2019-10-02 18:35:23 -0300

    Seller-DepartmentForeignKey

--- Models/Seller.cs
+++ Models/Seller.cs
@@ -13,6 +13,7 @@ namespace SalesWebMVC.Models
         public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
+        public int DepartmentId { get; set; }
         public ICollection<SalesRecord> SalesRecords { get; set; } = new List<SalesRecord>(); //Instanciar
         
         public Seller() { }
@@ -25,6 +26,7 @@ namespace SalesWebMVC.Models
             BirthDate = birthDate;
             BaseSalary = baseSalary;
             Department = department;
+            DepartmentId = department.Id;
         }
 
         public void AddSale(SalesRecord sr)
